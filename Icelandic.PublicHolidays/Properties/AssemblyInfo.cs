8290a81658c3800d2fbf6021a136377abbb2427a
HBI\bk <birgir@centara.com>
2018-04-29 20:29:22 +0000

    Version bump

--- Icelandic.PublicHolidays/Properties/AssemblyInfo.cs
+++ Icelandic.PublicHolidays/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6bf4b8ca-78d9-48f4-8719-bda0031fdd8b")]
 
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyVersion("0.9.1.0")]
+[assembly: AssemblyFileVersion("0.9.1.0")]
